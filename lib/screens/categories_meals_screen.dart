356630d05b1f9f38fe99e08ca01d91ca24657c64
AlexandreCorcos <acorcos@gmail.com>
2020-12-11 16:58:05 +0000

    first commit

---
+++ lib/screens/categories_meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cook Recipe'),
+      ),
+      body: Center(
+        child: Text('Category Recipe'),
+      ),
+    );
+  }
+}
